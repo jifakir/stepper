2e59b218977aee11351cf5ea70aba49eebcfd30b
jifakir <jifakir@gmail.com>
2021-06-18 20:40:02 +0600

    Updated

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,15 @@ export default function Home() {
 
   return (
     <div className="w-full min-h-screen flex">
-      <div className="w-full min-h-full flex flex-col justify-center items-center">
-        <h1 className="w-full text-center text-5xl">This is a form with stepper</h1>
-        <Link href='/form'><a className="py-10 text-blue-700 text-bold">Go to Form to see the form.</a></Link>
+      <div className="w-full min-h-full flex justify-center items-center">
+        <div className="border border-blue-500 p-5 m-2 rounded-md flex flex-col justify-center items-center">
+          <h1 className="w-full text-center text-5xl text-blue-500">Form</h1>
+          <Link href='/form'><a className="py-10 text-blue-700 text-bold">Go to Form to see the form.</a></Link>
+        </div>
+        <div className="border border-blue-500 p-5 m-2 rounded-md flex flex-col justify-center items-center">
+          <h1 className="w-full text-center text-5xl text-blue-500">Editorjs</h1>
+          <Link href='/editor'><a className="py-10 text-blue-700 text-bold">Go to Form to see the form.</a></Link>
+        </div>
       </div>
     </div>
   )
